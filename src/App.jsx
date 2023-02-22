0df58c3cb2e2aab00ce72ea45d0c6cdb28e9dec1
kamronov <kamronovk3@gmail.com>
2023-02-22 12:42:34 +0500

    gg

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import Benefit from './components/Benefit/Benefit';
 import Comment from './components/Comment/Comment';
 import Course from './components/Course/Course';
-import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Join from './components/Join/Join';
@@ -18,8 +17,6 @@ function App() {
       <Join/>
       <Benefit/>
       <Comment/>
-      
-    {/* <Footer/> */}
     </div>
   );
 }
